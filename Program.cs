0d5b92ce97ba721f80049567c08f6983c1f89297
gabriel esteves <gabriel-esteves09@hotmail.com>
2026-02-12 19:42:09 -0300

    Update

--- Program.cs
+++ Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "API Catálogo de Produtos", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Auth API", Version = "v1" });
 
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
